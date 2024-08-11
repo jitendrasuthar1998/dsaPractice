1803b09fe74880de21a997b25da40c3984b71b84
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2024-08-11 12:06:21 +0530

    added my understanding of factorial and trailingZeros questions in the comments form

--- maths/3.factorial.js
+++ maths/3.factorial.js
@@ -15,6 +15,14 @@ function factorial(n){
     for(let i=0;i<n;i++){
         res = res * (n - i);
     }
+
+    // i = 0 => res = 1 * (5 - 0) => res = 1 * 5 => 5
+    // i = 1 => res = 5 * (5 - 1) => res = 5 * 4 => 20
+    // i = 2 => res = 20 * (5 - 2) => res = 20 * 3 => 60
+    // i = 3 => res = 60 * (5 - 3) => res = 60 * 2 => 120
+    // i = 4 => res = 120 * (5 - 4) => res = 120 * 1 => 120
+    
+
     return res;
 }
 
@@ -25,6 +33,13 @@ function recursiveFactorial(n){
 
     return n * recursiveFactorial(n - 1);
 }
+
+// 5 * recursiveFactorial(5 - 1) => 5 * 4 * 3 * 2 * 1
+// 4 * recursiveFactorial(4 - 1) => 4 * 3 * 2 * 1
+// 3 * recursiveFactorial(3 - 1) => 3 * 2 * 1
+// 2 * recursiveFactorial(2 - 1) => 2 * 1
+// 1 * recursiveFactorial(1 - 1) => 1
+
 
 console.log("factorial of 5 is using recursive == ", recursiveFactorial(5))
 
--- maths/4.trailingZeros.js
+++ maths/4.trailingZeros.js
@@ -11,15 +11,51 @@ trailing zero = 2
 formula for trailing zero = n/5 + n/25 + n/125 + ...
 */ 
 
+
+// given number = 4
+
+// factorial of given number => 4 * 3 * 2 * 1 = 24
+
+// trailing zeros in the factorial of 4 => 0
+
+// given number = 10
+
+// factorial of given number => 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1  = 36,28,800
+
+// trailing zeros in the factorial of 4 => 0
+
+
 function countTrailingZeroes(number){
     let res = 0;
 
     for(let i=5;i<=number;i=i*5){
         res = res + Math.floor(number/i);
     }
+
+    // number = 10
+
+    // i = 5 => res = res + Math.floor(number/i) => res = 0 + Math.floor(10/5) => 2
+    
+    // i = 25 => i <= number => condition false, will not get into for loop.
+
+
+
     return res;
 }
 
 console.log("trailing zero in factorial of 5 is == ", countTrailingZeroes(5));
 
 console.log("trailing zero in factorial of 3455 is == ", countTrailingZeroes(3455));
+
+// number = 3455
+
+// i = 5 => res = res + Math.floor(number/i) => res = 0 + Math.floor(3455/5) => 691
+    
+// i = 25 => res = 691 + Math.floor(3455/25) => 691 + 138 =>  829
+
+// i = 125 => 829 + Math.floor(3455/125) => 829 + 27 => 856
+
+// i = 625 => 856 + Math.floor(3455/625) => 856 + 5 => 861
+
+// i = 3125 => 861 + Math.floor(3455/3125) => 861 + 1 => 862
+ 
